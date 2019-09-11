6fa270c140848ed0909a8a30cd5c8cff274d7023
Pinkbowser <lletterwinkel2@gmail.com>
2019-09-11 08:43:07 +0200

    Versie 1.0 Werkt

--- src/com/github/squadalpha/TicTacToeUitprinter.java
+++ src/com/github/squadalpha/TicTacToeUitprinter.java
@@ -8,48 +8,50 @@ class TicTacToeUitprinter {
         new TicTacToeUitprinter().run();
     }
 
-    Scanner WelkeNummer = new Scanner(System.in);
+    Scanner welkeNummer = new Scanner(System.in);
+    Integer maxZetten = 0;
+    String winst;
 
-    Integer MaxZetten = 0;
-    Integer Winst1 = 0;
-    Integer Winst2 = 0;
-
-   String[] bordPlekken = new String[]{"0","1","2","3","4","5","6","7","8" };
+    String[] bordPlekken = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8"};
 
     public void run() {
 
 
         while (true) {
 
-            this.PrintBordUit();//Print het bord uit
+            this.printBordUit();//Print het bord uit
 
-            this.ZetInput1();//Neemt de input van Speler 1 en zet die op het bord\
+            this.zetInput("O");//Neemt de input van Speler 1 en zet die op het bord
 
-            this.WinstSpeler1();
+            this.winstSpeler("O");
 
-            if (Winst1 == 1) break;
+            if (winst == "O") {
+                this.printBordUit();
+                break;
+            }
 
-            if (MaxZetten > 9) break;
+            if (maxZetten == 9) {
+                this.printBordUit();
+                break;
+            }
 
-            this.PrintBordUit();//Print het bord uit
+            this.printBordUit();//Print het bord uit
 
-            this.ZetInput2();//Neemt de input van Speler 2 en zet die op het bord
+            this.zetInput("X");//Neemt de input van Speler 2 en zet die op het bord
 
-            this.WinstSpeler2();
+            this.winstSpeler("O");
 
-            if (Winst2 == 1) break;
-
-            if (MaxZetten > 9) break;
+            if (winst == "X") break;
 
         }
-        if (Winst1 == 1) System.out.println("Speler 1 heeft gewonnen");
-        else if (Winst2 == 1) System.out.println("Speler 2 heeft gewonnen");
+        if (winst == "O") System.out.println("Speler O heeft gewonnen");
+        else if (winst == "X") System.out.println("Speler X heeft gewonnen");
         else System.out.println("Gelijkspel");
 
 
     }
 
-    public void PrintBordUit() {
+    public void printBordUit() {
         System.out.println(bordPlekken[0] + "|" + bordPlekken[1] + "|" + bordPlekken[2]); //Print het bord
         System.out.println("-----");
         System.out.println(bordPlekken[3] + "|" + bordPlekken[4] + "|" + bordPlekken[5]);
@@ -57,82 +59,74 @@ class TicTacToeUitprinter {
         System.out.println(bordPlekken[6] + "|" + bordPlekken[7] + "|" + bordPlekken[8]);
     }
 
-    public void ZetInput1() {
-        boolean zetgoed = false;
-        while(!zetgoed){
-        System.out.println("Speler 1");//Moet de variable aanpassen om de move te registreren
-        String ImputSpeler1 = WelkeNummer.nextLine();
-        if (ImputSpeler1.equals("0") && bordPlekken[0].equals("0")) {bordPlekken[0] = ("O");zetgoed = true;}
-        if (ImputSpeler1.equals("1") && bordPlekken[1].equals("1")) {bordPlekken[1] = ("O");zetgoed = true;}
-        if (ImputSpeler1.equals("2") && bordPlekken[2].equals("2")) {bordPlekken[2] = ("O");zetgoed = true;}
-        if (ImputSpeler1.equals("3") && bordPlekken[3].equals("3")) {bordPlekken[3] = ("O");zetgoed = true;}
-        if (ImputSpeler1.equals("4") && bordPlekken[4].equals("4")) {bordPlekken[4] = ("O");zetgoed = true;}
-        if (ImputSpeler1.equals("5") && bordPlekken[5].equals("5")) {bordPlekken[5] = ("O");zetgoed = true;}
-        if (ImputSpeler1.equals("6") && bordPlekken[6].equals("6")) {bordPlekken[6] = ("O");zetgoed = true;}
-        if (ImputSpeler1.equals("7") && bordPlekken[7].equals("7")) {bordPlekken[7] = ("O");zetgoed = true;}
-        if (ImputSpeler1.equals("8") && bordPlekken[8].equals("8")) {bordPlekken[8] = ("O");zetgoed = true;}}
-        ++MaxZetten;//Doet een move bij de counter
+    public void zetInput(String playerName) {
+        boolean zetGoed = false;
+        while (!zetGoed) {
+            System.out.println("Speler " + playerName);//Moet de variable aanpassen om de move te registreren
+            String ImputSpeler = welkeNummer.nextLine();
+            if (ImputSpeler.equals("0") && bordPlekken[0].equals("0")) {
+                bordPlekken[0] = (playerName);
+                zetGoed = true;
+            } else if (ImputSpeler.equals("1") && bordPlekken[1].equals("1")) {
+                bordPlekken[1] = (playerName);
+                zetGoed = true;
+            } else if (ImputSpeler.equals("2") && bordPlekken[2].equals("2")) {
+                bordPlekken[2] = (playerName);
+                zetGoed = true;
+            } else if (ImputSpeler.equals("3") && bordPlekken[3].equals("3")) {
+                bordPlekken[3] = (playerName);
+                zetGoed = true;
+            } else if (ImputSpeler.equals("4") && bordPlekken[4].equals("4")) {
+                bordPlekken[4] = (playerName);
+                zetGoed = true;
+            } else if (ImputSpeler.equals("5") && bordPlekken[5].equals("5")) {
+                bordPlekken[5] = (playerName);
+                zetGoed = true;
+            } else if (ImputSpeler.equals("6") && bordPlekken[6].equals("6")) {
+                bordPlekken[6] = (playerName);
+                zetGoed = true;
+            } else if (ImputSpeler.equals("7") && bordPlekken[7].equals("7")) {
+                bordPlekken[7] = (playerName);
+                zetGoed = true;
+            } else if (ImputSpeler.equals("8") && bordPlekken[8].equals("8")) {
+                bordPlekken[8] = (playerName);
+                zetGoed = true;
+            } else System.out.println("Verkeerde move, Probeer opnieuw");
+        }
+        ++maxZetten;//Doet een move bij de counter
     }
 
-    public void ZetInput2() {
-        boolean zetgoed = false;
-        while(!zetgoed){
-        System.out.println("Speler 2");//Moet de variable aanpassen om de move te registreren
-        String ImputSpeler2 = WelkeNummer.nextLine();
-        if (ImputSpeler2.equals("0")&& bordPlekken[0].equals("0")) {bordPlekken[0] = ("X");zetgoed = true;}
-        if (ImputSpeler2.equals("1")&& bordPlekken[1].equals("1")) {bordPlekken[1] = ("X");zetgoed = true;}
-        if (ImputSpeler2.equals("2")&& bordPlekken[2].equals("2")) {bordPlekken[2] = ("X");zetgoed = true;}
-        if (ImputSpeler2.equals("3")&& bordPlekken[3].equals("3")) {bordPlekken[3] = ("X");zetgoed = true;}
-        if (ImputSpeler2.equals("4")&& bordPlekken[4].equals("4")) {bordPlekken[4] = ("X");zetgoed = true;}
-        if (ImputSpeler2.equals("5")&& bordPlekken[5].equals("5")) {bordPlekken[5] = ("X");zetgoed = true;}
-        if (ImputSpeler2.equals("6")&& bordPlekken[6].equals("6")) {bordPlekken[6] = ("X");zetgoed = true;}
-        if (ImputSpeler2.equals("7")&& bordPlekken[7].equals("7")) {bordPlekken[7] = ("X");zetgoed = true;}
-        if (ImputSpeler2.equals("8")&& bordPlekken[8].equals("8")) {bordPlekken[8] = ("X");zetgoed = true;}}
-        ++MaxZetten;//Doet een move bij de counter
+
+    public void winstSpeler(String playerName) {
+        if (bordPlekken[0].equals(playerName) && (bordPlekken[1].equals(playerName)) && (bordPlekken[2].equals(playerName))) {
+            winst = playerName;
+        }
+        if (bordPlekken[3].equals(playerName) && (bordPlekken[4].equals(playerName)) && (bordPlekken[5].equals(playerName))) {
+            winst = playerName;
+        }
+        if (bordPlekken[6].equals(playerName) && (bordPlekken[7].equals(playerName)) && (bordPlekken[8].equals(playerName))) {
+            winst = playerName;
+        }
+
+        if (bordPlekken[0].equals(playerName) && (bordPlekken[3].equals(playerName)) && (bordPlekken[6].equals(playerName))) {
+            winst = playerName;
+        }
+        if (bordPlekken[1].equals(playerName) && (bordPlekken[4].equals(playerName)) && (bordPlekken[7].equals(playerName))) {
+            winst = playerName;
+        }
+        if (bordPlekken[8].equals(playerName) && (bordPlekken[7].equals(playerName)) && (bordPlekken[6].equals(playerName))) {
+            winst = playerName;
+        }
+
+        if (bordPlekken[0].equals(playerName) && (bordPlekken[4].equals(playerName)) && (bordPlekken[8].equals(playerName))) {
+            winst = playerName;
+        }
+        if (bordPlekken[2].equals(playerName) && (bordPlekken[4].equals(playerName)) && (bordPlekken[6].equals(playerName))) {
+            winst = playerName;
+        }
     }
 
-    public void WinstSpeler1() {
-        if (bordPlekken[0].equals("O") && (bordPlekken[1].equals("O")) && (bordPlekken[2].equals("O"))) {Winst1 = 1;
-        System.out.println("Winst speler 1");}
-        if (bordPlekken[3].equals("O") && (bordPlekken[4].equals("O")) && (bordPlekken[5].equals("O"))) {Winst1 = 1;
-        System.out.println("Winst speler 1");}
-        if (bordPlekken[6].equals("O") && (bordPlekken[7].equals("O")) && (bordPlekken[8].equals("O"))) {Winst1 = 1;
-        System.out.println("Winst speler 1");}
 
-        if (bordPlekken[0].equals("O") && (bordPlekken[3].equals("O")) && (bordPlekken[6].equals("O"))) {Winst1 = 1;
-        System.out.println("Winst speler 1");}
-        if (bordPlekken[1].equals("O") && (bordPlekken[4].equals("O")) && (bordPlekken[7].equals("O"))) {Winst1 = 1;
-        System.out.println("Winst speler 1");}
-        if (bordPlekken[8].equals("O") && (bordPlekken[7].equals("O")) && (bordPlekken[6].equals("O"))) {Winst1 = 1;
-        System.out.println("Winst speler 1");}
-
-        if (bordPlekken[0].equals("O") && (bordPlekken[4].equals("O")) && (bordPlekken[8].equals("O"))) {Winst1 = 1;
-        System.out.println("Winst speler 1");}
-        if (bordPlekken[2].equals("O") && (bordPlekken[4].equals("O")) && (bordPlekken[6].equals("O"))) {Winst1 = 1;
-        System.out.println("Winst speler 1");}
-    }
-
-    public void WinstSpeler2() {
-        if (bordPlekken[0].equals("X") && (bordPlekken[1].equals("X")) && (bordPlekken[2].equals("X"))) {Winst2 = 1;
-        System.out.println("Winst speler 2");}
-        if (bordPlekken[3].equals("X") && (bordPlekken[4].equals("X")) && (bordPlekken[5].equals("X"))) {Winst2 = 1;
-        System.out.println("Winst speler 2");}
-        if (bordPlekken[6].equals("X") && (bordPlekken[7].equals("X")) && (bordPlekken[8].equals("X"))) {Winst2 = 1;
-        System.out.println("Winst speler 2");}
-
-        if (bordPlekken[0].equals("X") && (bordPlekken[3].equals("X")) && (bordPlekken[6].equals("X"))) {Winst2 = 1;
-        System.out.println("Winst speler 2");}
-        if (bordPlekken[1].equals("X") && (bordPlekken[4].equals("X")) && (bordPlekken[7].equals("X"))) {Winst2 = 1;
-        System.out.println("Winst speler 2");}
-        if (bordPlekken[8].equals("X") && (bordPlekken[7].equals("X")) && (bordPlekken[6].equals("X"))) {Winst2 = 1;
-        System.out.println("Winst speler 2");}
-
-        if (bordPlekken[0].equals("X") && (bordPlekken[4].equals("X")) && (bordPlekken[8].equals("X"))) {Winst2 = 1;
-        System.out.println("Winst speler 2");}
-        if (bordPlekken[2].equals("X") && (bordPlekken[4].equals("X")) && (bordPlekken[6].equals("X"))) {Winst2 = 1;
-        System.out.println("Winst speler 2");}
-
-
-    }
 }
+
 
